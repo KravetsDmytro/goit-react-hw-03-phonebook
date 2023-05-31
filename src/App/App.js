a7557fed1a2c7694eb73266b9a1250886c9cda3e
Kravets_Dmytro <ditur@ukr.net>
2023-05-31 13:43:21 +0300

    src+

---
+++ src/App/App.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import ContactForm from '../components/contactForm/ContactForm';
+import ContactList from '../components/contactList/ContactList.js';
+import Filter from '../components/ContactFilter/ContactFiltr';
+import css from './App.module.css';
+
+
+// import ContactUs from './components/contactUs/ContactUs';
+// import Login from './components/login/Login';
+// import Phone from './components/phone/phone.js' ;
+
+
+class App extends React.Component {
+  state = {
+    contacts:  [
+      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+    ],
+    filter: '',
+  }
+
+  // Додаємо контакт
+  addContact = newContact => {
+    // методперевірки  контакта чи не повторюється.
+    const duplicateName = this.state.contacts.find(
+      contact => contact.name === newContact.name
+    );
+
+    if (duplicateName) {
+      alert(`${newContact.name} is already on contacts`);
+      return;
+    }
+
+    this.setState(({ contacts }) => ({
+      contacts: [newContact, ...contacts],
+    }));
+  };
+
+  // фільтр та  записуємо у стейт
+  changeFilter = event => {
+    this.setState({ filter: event.currentTarget.value });
+  };
+
+  // Фільтруємо контакти та повертаємо результат фільтру
+  filterContacts = () => {
+    const { contacts, filter } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+
+
+// видаленя контакту
+deleteContact = contactId =>{
+this.setState(prevState =>({contacts:
+   prevState.contacts.filter(contact => contact.id !==contactId),}));
+};
+
+
+  render() {
+const {contacts} = this.state;
+const { filter } = this.state;
+const filteredResults = this.filterContacts();
+
+    return (
+      <div className={css.container} >
+        <div>Книга контактів</div>
+<ContactForm onSubmit={this.addContact}  />
+<Filter value={filter} onChange={this.changeFilter} />
+<ContactList contacts={filteredResults} onDeleteContact= {this.deleteContact} />
+<div>
+<p>
+Загальна кількість контактів: {contacts.length}
+</p>
+{/* <p>Кільквість записаних контактів:{completedContact} </p> */}
+  </div>
+
+        {/* <Phone />
+
+        <ContactUs />
+        <Login /> */}
+
+      </div>
+    )
+  }
+}
+
+export default App;
